3f9ca93a424d9683a7047339274510a2d25a6d48
Fremen1990 <fremen1990@gmail.com>
2022-02-05 03:03:44 +0100

    Update CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // ==========================  SHOW MENU =============================//
 
 const ShowMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
 
-  // Validate that variable exist
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      // We add the show-menu class to the div with nav__menu class
-      nav.classList.toggle("show-menu");
-    });
-  }
+    // Validate that variable exist
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            // We add the show-menu class to the div with nav__menu class
+            nav.classList.toggle("show-menu");
+        });
+    }
 };
 
 ShowMenu("nav-toggle", "nav-menu");
@@ -20,10 +20,10 @@ ShowMenu("nav-toggle", "nav-menu");
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
+    const navMenu = document.getElementById("nav-menu");
 
-  //When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show-menu");
+    //When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu");
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
@@ -33,32 +33,32 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-  const scrollY = window.pageYOffset;
+    const scrollY = window.pageYOffset;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__manu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__manu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(".nav__manu a[href*=" + sectionId + "]")
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(".nav__manu a[href*=" + sectionId + "]")
+                .classList.remove("active-link");
+        }
+    });
 }
 
 // ==================== SHOW SCROLL TOP =========================//
 
 function scrollTop() {
-  const scrollTop = document.getElementById("scroll-top");
-  //When th scroll is higher than 560 viewporrt heigh, add the show-scroll class to the tag with scroll
-  if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
-  else scrollTop.classList.remove("show-scroll");
+    const scrollTop = document.getElementById("scroll-top");
+    //When th scroll is higher than 560 viewporrt heigh, add the show-scroll class to the tag with scroll
+    if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
+    else scrollTop.classList.remove("show-scroll");
 }
 
 window.addEventListener("scroll", scrollTop);
@@ -71,10 +71,10 @@ const iconTheme = "bx-sun";
 
 // buttons class active for different theme and download different CV
 const downloadDarkResumeButton = document.getElementById(
-  "home__button-movil-dark"
+    "home__button-movil-dark"
 );
 const downloadLightResumeButton = document.getElementById(
-  "home__button-movil-light"
+    "home__button-movil-light"
 );
 const homeButtonMovilActive = "home__button-movil-active";
 
@@ -84,81 +84,92 @@ const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
+    document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+    themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  //If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark theme
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
-    iconTheme
-  );
+    //If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark theme
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+    );
+    themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+        iconTheme
+    );
 }
 //Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-  //Add or remove the dark/icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
+    //Add or remove the dark/icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
 
-  // ================================ DOWNLOAD = PDF ON MOBILE ==================================//
-  downloadDarkResumeButton.classList.toggle(homeButtonMovilActive);
-  downloadLightResumeButton.classList.toggle(homeButtonMovilActive);
+    // ================================ DOWNLOAD = PDF ON MOBILE ==================================//
+    downloadDarkResumeButton.classList.toggle(homeButtonMovilActive);
+    downloadLightResumeButton.classList.toggle(homeButtonMovilActive);
 
-  //We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+    //We save the theme and the current icon that the user chose
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
 // ======================= DEFAULT THEME DARK ==================================
 function defaultTheme() {
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
 }
+
 window.onload = defaultTheme();
 
 // ================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ==================//
 function scaleCv() {
-  document.body.classList.add("scale-cv");
+    document.body.classList.add("scale-cv");
 }
+
+
 // ================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ==================//
 function removeScale() {
-  document.body.classList.remove("scale-cv");
+    document.body.classList.remove("scale-cv");
 }
 
 // ================================ GENERATE PDF ==================================//
 // PDF generated area
-let areaCv = document.getElementById("area-cv");
+let areaCv = document.getElementById('area-cv');
 
-let resumeButton = document.getElementById("resume-button");
 
 // Html2pdf option
 let opt = {
-  margin: 0,
-  filename: "Tomasz Stanisz - Resume.pdf",
-  image: { type: "jpeg", quality: 0.98 },
-  html2canvas: { scale: 4 },
-  jsPDF: { format: "a4", orientation: "portrait" },
+    margin: 0,
+    filename: "Tomasz Stanisz - Resume.pdf",
+    image: {type: "jpeg", quality: 0.98},
+    html2canvas: {scale: 4},
+    jsPDF: {unit: 'in', format: "a4", orientation: "portrait"},
 };
+
+let resumeButton = document.getElementById("resume-button");
+
+// const element = document.getElementById('element-to-print');
 
 // Function t call areaCv and Html2Pdf options
 function generateResume() {
-  console.log("generateResume functions");
-  html2pdf(areaCv, opt);
+    console.log("generateResume functions");
+    html2pdf(areaCv, opt);
 }
+
 
 // When the button is clicked, it exectutes the three functions
 resumeButton.addEventListener("click", () => {
-  // 1. THe class .scale-cv is added to the body, where it reduces the size
-  scaleCv();
+    // 1. THe class .scale-cv is added to the body, where it reduces the size
+    scaleCv();
 
-  // 2. The PDF is generated
-  generateResume();
+    // 2. The PDF is generated
+    generateResume();
+    console.log("Clicked donwload")
+    // html2pdf().from(areaCv).save();
 
-  // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal view
+    // html2pdf(areaCv);
 
-  setTimeout(removeScale, 1000);
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal view
+
+    setTimeout(removeScale, 2000);
 });
